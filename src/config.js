971b3cc859d9df6b9996223db760b49d2ad81a92
Eleftherios Chetzakis <eleftherios@workable.com>
2019-08-09 16:50:43 +0300

    Config tests

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 /* eslint-disable no-process-env */
-const path = require('path');
-const configFileName = '.pg-doc.json';
-const configPath = process.cwd();
-const configFullPath = path.join(configPath, configFileName);
+const rcLoader = require('./rc-loader');
 const config = {
     toc: true,
     splitByInitial: true,
@@ -12,20 +9,17 @@ const config = {
 };
 
 // From .pg-doc.json
-try {
-    let rc = require(configFullPath);
-    Object.assign(config, rc);
-} catch (e) {
-    // don't care
-}
+const opts = rcLoader.loadRcFile();
+Object.assign(config, opts);
 
 // From env
 const env = {
+    toc: process.env.PGDOC_TOC ? process.env.PGDOC_TOC === 'true' : null,
     connection: process.env.PGDOC_CONNECTION,
     out: process.env.PGDOC_OUT,
     title: process.env.PGDOC_TITLE,
     excluded: process.env.PGDOC_EXCLUDED ? process.env.PGDOC_EXCLUDED.split(',').map(s => s.trim()) : null,
-    splitLimit: Number.isInteger(process.env.PGDOC_SPLIT_LIMIT) ? parseInt(process.env.PGDOC_SPLIT, 10) : null,
+    splitLimit: parseInt(process.env.PGDOC_SPLIT_LIMIT, 10) || null,
     splitByInitial: process.env.PGDOC_SPLIT_BY_INITIAL ? process.env.PGDOC_SPLIT_BY_INITIAL === 'true' : null
 };
 
---
+++ test/src/config.test.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-process-env */
+const sandbox = sinon.createSandbox();
+const rcLoader = require('../../src/rc-loader');
+const defaults = {
+    toc: true,
+    splitByInitial: true,
+    splitLimit: 20,
+    title: 'Database Documentation',
+    out: 'DATABASE.md'
+};
+const envDefaults = {
+    PGDOC_TOC: false,
+    PGDOC_CONNECTION: 'PGDOC_CONNECTION',
+    PGDOC_OUT: 'PGDOC_OUT',
+    PGDOC_TITLE: 'PGDOC_TITLE',
+    PGDOC_EXCLUDED: 'PGDOC_EXCLUDED_1, PGDOC_EXCLUDED_2',
+    PGDOC_SPLIT_LIMIT: '100',
+    PGDOC_SPLIT_BY_INITIAL: 'false'
+};
+
+describe('config test', () => {
+    beforeEach(() => {
+        delete require.cache[require.resolve('../../src/config')];
+    });
+    afterEach(() => {
+        sandbox.restore();
+    });
+    describe('when rc file exist', () => {
+        describe('when all options defined', () => {
+            it('should override all options', () => {
+                sandbox.stub(rcLoader, 'loadRcFile').returns(fixtures.opts.full);
+                const config = require('../../src/config');
+                config.should.be.eql(fixtures.opts.full);
+            });
+        });
+        describe('when some options defined', () => {
+            it('should override only provided options', () => {
+                sandbox.stub(rcLoader, 'loadRcFile').returns(fixtures.opts.partial);
+                const expected = Object.assign({}, defaults, fixtures.opts.partial);
+                const config = require('../../src/config');
+                config.should.be.eql(expected);
+            });
+        });
+    });
+
+    describe('when env variables are set', () => {
+        afterEach(() => {
+            unmockEnv();
+        });
+        describe('when all options defined', () => {
+            it('should replace defaults', () => {
+                mockEnv(envDefaults);
+                const config = require('../../src/config');
+                config.should.be.eql(fixtures.env.full);
+            });
+        });
+        describe('when some options defined', () => {
+            it('should override only provided options', () => {
+                const opts = {
+                    PGDOC_CONNECTION: 'PGDOC_CONNECTION',
+                    PGDOC_OUT: 'PGDOC_OUT',
+                    PGDOC_TITLE: 'PGDOC_TITLE'
+                };
+                mockEnv(opts);
+                const config = require('../../src/config');
+                config.should.be.eql(fixtures.env.partial);
+            });
+        });
+    });
+});
+
+function mockEnv(opts) {
+    const vars = Object.assign({}, opts);
+    Object.keys(vars).forEach(variable => {
+        process.env[variable] = vars[variable];
+    });
+}
+
+function unmockEnv() {
+    Object.keys(envDefaults).forEach(variable => {
+        delete process.env[variable];
+    });
+}
